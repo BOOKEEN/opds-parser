914ae28b5e88386db73bc23a0831b6babf281ac1
Melanie Vadde <melaniev@bookeen.com>
2018-06-25 10:31:40 +0200

    MBW-1827 missing files

---
+++ src/Entity/Navigation.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace OpdsBundle\Entity;
+
+class Navigation extends Link
+{
+    /**
+     * @var string 
+     */
+    private $content;
+
+    /**
+     * @var string 
+     */
+    private $id;
+
+    /**
+     * @var \DateTime 
+     */
+    private $updatedAt;
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+}
---
+++ src/Exception/OpdsParserLoaderException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OpdsBundle\Exception;
+
+class OpdsParserLoaderException
+{
+    
+}
